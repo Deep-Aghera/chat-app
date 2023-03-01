498f9305d30faec85f32b031273a9280c5abc8dd
Deep Aghera <deep.aghera@marutitech.com>
2023-03-01 11:33:32 +0530

    acknolagement

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,27 @@
-io()
+const socket = io()
+
+
+    socket.on('message',(msg) => {
+        console.log("from server",msg)
+    })
+
+    document.getElementById('sendbtn').addEventListener('click',() => {
+        let msg = document.getElementById('inputbox').value;
+        console.log(msg);
+        socket.emit('sendmsg',msg,(message) => {
+            console.log("The message was delivered!",message);
+        });
+    })
+
+    document.querySelector('#send-loacation').addEventListener('click',() => {
+        if(!navigator.geolocation) {
+            return alert('Geolocation is not supported by your browser');
+        }
+        navigator.geolocation.getCurrentPosition((postion) => {
+            console.log(postion);
+            socket.emit('sendLocation',{
+                latitude : postion.coords.latitude,
+                longitude : postion.coords.longitude
+            })
+        })
+    })
--- src/index.js
+++ src/index.js
@@ -11,9 +11,27 @@ const io = socketio(server)
 const publicDirectoryPath = path.join(__dirname,'../public');
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection',() => {
-    console.log("New webSocket connection")
+
+io.on('connection',(socket) => {
+    console.log("New webSocket connection");
+
+    socket.emit('message','Welcome!')
+    socket.broadcast.emit('message','A new user has joined!')
+    socket.on('sendmsg',(msg,callback) => {
+        console.log(msg)
+        io.emit('message',msg)
+        callback('Delivered!');
+    });
+
+    socket.on('sendLocation',(coords) => {
+        io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+
+    socket.on('disconnect',() => {
+        io.emit('message','A user has left!');
+    })
 })
+
 
 server.listen(3000,() => {
     console.log("server listn at 3000")
